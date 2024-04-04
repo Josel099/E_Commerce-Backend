1363221dd87a1af8cdcb1e062a71efdb70caf964
DESKTOP-2STDA9J\Livares <josel.x@livares.com>
2024-04-04 15:09:51 +0530

    return type changed completely

--- src/main/java/com/livares/product/controller/CartController.java
+++ src/main/java/com/livares/product/controller/CartController.java
@@ -1,14 +1,17 @@
 
 package com.livares.product.controller;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.livares.product.Dto.ProductDTO;
-import com.livares.product.model.Product;
 import com.livares.product.response.ResponseHandler;
 import com.livares.product.service.UserCartService;
 
