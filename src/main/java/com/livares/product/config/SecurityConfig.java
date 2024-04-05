1da903f2543e8e3cf2656041be5b4f395fcc8893
DESKTOP-2STDA9J\Livares <josel.x@livares.com>
2024-04-05 18:02:59 +0530

    In memmory authentication done

--- src/main/java/com/livares/product/config/SecurityConfig.java
+++ src/main/java/com/livares/product/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -22,11 +26,36 @@ public class SecurityConfig {
      * @throws Exception If an error occurs while configuring the security filter chain
      */
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
+   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf(csrf -> csrf.disable());
+//    	  return http.build();
+    	
+    	
+    	return http.authorizeHttpRequests(registry->{
+    			registry.requestMatchers("/home").permitAll();           
+    			registry.requestMatchers("/admin/**").hasRole("ADMIN");
+    			registry.requestMatchers("/user/**").hasRole("USER");
+    			registry.anyRequest().authenticated();  // any request other than the specified one's that's requires authentication. 
+    	})
+    			.formLogin(formLogin->formLogin.permitAll()) // customizing the  default login page for permitting for all request. 
+    			.build();
     }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+    	UserDetails normalUser = User.builder()
+    								.username("user")
+    								.password("$2a$12$08k7tqqv.q1WI/F6SDtsJesu/niAI.CTY0T2pz/cXeUbnQi1LNREa")
+    								.roles("USER")
+    								.build();
+    	UserDetails adminUser = User.builder()
+				.username("admin")
+				.password("$2a$12$4vGptdTAo1NBcWWMw1tQdunepMbztRs/e8eAcW6VZZK2hKPow8aby")
+				.roles("USER","ADMIN")
+				.build();
+    	return new InMemoryUserDetailsManager(normalUser,adminUser);
+    }
+    
     
     
     
---
+++ src/main/java/com/livares/product/controller/ContentController.java
@@ -0,0 +1,24 @@
+package com.livares.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContentController {
+
+	@GetMapping("/home")
+	public String handleWelcome() {
+		return "welcome home";
+	}
+	
+	@GetMapping("/admin/home")
+	public String handleAdminHome() {
+		return "home admin";
+	}
+	
+		@GetMapping("/user/home")
+	public String handleUserHome() {
+		return "user Home";
+	}
+	
+}
