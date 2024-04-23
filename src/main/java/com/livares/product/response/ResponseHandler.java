31cb14348612196d99a7df208628acb43f5e7c26
Josel Xavier \Livares <josel.x@livares.com>
2024-04-23 14:25:47 +0530

    made changes in response handler

--- src/main/java/com/livares/product/response/ResponseHandler.java
+++ src/main/java/com/livares/product/response/ResponseHandler.java
@@ -1,30 +1,18 @@
 package com.livares.product.response;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseHandler<T> {
-//
-//    public static CustomResponse generateResponse(CustomResponse response, Object data,
-//            String successMessage, String errorMessage) {
-//        if (data != null) {
-//            response.setSuccess(true);
-//            response.setData(data);
-//            response.setMessage(successMessage);
-//        } else {
-//            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, errorMessage);
-//        }
-//        return response;
-//    }
 
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status,
+        Object responseObj) {
 
-    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
-       
-    	CustomResponse<Object> response = new CustomResponse<Object>();
-    	response.setMessage(message);
-    	response.setStatus(status);
-    	response.setData(responseObj);
-            return new ResponseEntity<Object>(response,status);
+        CustomResponse<Object> response = new CustomResponse<Object>();
+        response.setMessage(message);
+        response.setStatus(status);
+        response.setData(responseObj);
+        return new ResponseEntity<Object>(response, status);
     }
 
-
-} 
+}
